cc631d578b9365cdbd5fd086ba7b26c811c6d859
mourad5000 <mouradmounim7@gmail.com>
2021-05-19 20:22:22 +0200

    Add color to header

--- front-developers-games/src/components/Header/Header.jsx
+++ front-developers-games/src/components/Header/Header.jsx
@@ -7,6 +7,14 @@ import headerLogos from '../../assets/images/header-logos.svg';
 import openMenuIcon from '../../assets/images/menu-icon.svg';
 // import closeMenuIcon from '../../assets/images/close-menu-icon.svg';
 
+const navigation = ['nav1', 'nav2', 'nav3'];
+
+const renderNavigation = (
+  navigation.map((element) => (
+    <p>{element}</p>
+  ))
+);
+
 function Header() {
   return (
     <header className="header">
@@ -15,8 +23,13 @@ function Header() {
           <img className="menu__logo" src={headerLogos} alt="Developers games Redhut and Santander logos" />
         </Link>
         <img src={openMenuIcon} alt="Open menu icon" />
-        <nav className="menu__navigation" />
+        <nav className="menu__navigation">
+          {renderNavigation}
+        </nav>
       </div>
+      {/* <div className="header__hamburger-menu mobile" style={transform: scaleX(0)}>
+
+      </div> */}
     </header>
   );
 }
