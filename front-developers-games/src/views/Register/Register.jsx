6733d3814ca94f86b198d0fab52c5c9646971149
mourad5000 <mouradmounim7@gmail.com>
2021-05-21 20:05:42 +0200

    Add TODOS in Register compo

--- front-developers-games/src/views/Register/Register.jsx
+++ front-developers-games/src/views/Register/Register.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
 import AppWrapper from '../../components/AppWrapper/AppWrapper';
@@ -17,13 +18,25 @@ const participantInitialState = {
 };
 
 function Register() {
-  const [registerForm, setRegisterForm] = useState(teamInitialState);
+  const [teamRegister, setTeamRegister] = useState(teamInitialState);
+  const [participantRegister, setParticipantRegister] = useState(participantInitialState);
+  // TODO: crear warningMessage
+  function handleTextInputChange({ target: { name, value } }) {
+    //   TODO: guardar el valor en el state que le pertoca
+    console.log(name);
+    console.log(value);
+  }
+
+  function handleCreateTeamClick() {
+    //   TODO: validacion preparar datos y dispatch action que envie al back
+  }
   return (
     <AppWrapper title="Inscribe a tu equipo">
       <section className="register__container">
         <h3>Tu equipo debe estar compuesto de un mínimo de 3 y un máximo de 4 participantes.</h3>
         <form>
-          <Input type="text" name="team-name" placeholder="Nombre del equipo*" />
+          <Input type="text" name="name" placeholder="Nombre del equipo*" value={teamInitialState.name} onChange={handleTextInputChange} />
+          {/* TODO: Pintar html */}
         </form>
       </section>
     </AppWrapper>
