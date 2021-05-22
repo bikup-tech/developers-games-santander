252ecc80da6896418875c13aad437290d52022be
mourad5000 <mouradmounim7@gmail.com>
2021-05-22 10:25:18 +0200

    Add style to register

--- front-developers-games/src/views/Register/Register.jsx
+++ front-developers-games/src/views/Register/Register.jsx
@@ -1,5 +1,10 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+
+import './Register.scss';
+
+import CamaraIcon from '../../assets/images/camara-icon.svg';
 
 import AppWrapper from '../../components/AppWrapper/AppWrapper';
 import Input from '../../components/Input/Input';
@@ -32,10 +37,17 @@ function Register() {
   }
   return (
     <AppWrapper title="Inscribe a tu equipo">
-      <section className="register__container">
-        <h3>Tu equipo debe estar compuesto de un mínimo de 3 y un máximo de 4 participantes.</h3>
-        <form>
-          <Input type="text" name="name" placeholder="Nombre del equipo*" value={teamInitialState.name} onChange={handleTextInputChange} />
+      <section className="register-container">
+        <h3 className="register__title">Tu equipo debe estar compuesto de un mínimo de 3 y un máximo de 4 participantes.</h3>
+        <form className="register__form">
+          <div className="form__input">
+            <Input type="text" name="name" placeholder="Nombre del equipo*" value={teamInitialState.name} onChange={handleTextInputChange} />
+          </div>
+          <div className="form__entry-photo">
+            <img className="entry-photo__image" src={CamaraIcon} alt="Insert photo" />
+            <p className="entry-photo__text">Sube la foto de tu equipo</p>
+          </div>
+          <h3 className="register__title">Participante 1 - Capitán</h3>
           {/* TODO: Pintar html */}
         </form>
       </section>
