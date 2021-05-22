dcbc37f1730061a11e35eb71830b87e3d6a5a6e5
mourad5000 <mouradmounim7@gmail.com>
2021-05-22 12:09:24 +0200

    Create the participant form

--- front-developers-games/src/views/Register/Participant/Participant.jsx
+++ front-developers-games/src/views/Register/Participant/Participant.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
+import './Participant.scss';
+
+import Input from '../../../components/Input/Input';
+
 function Participant({ text, participantNumber, isCaptain }) {
+  function handleTextInputChange({ target: { name, value } }) {
+    //   TODO: guardar el valor en el state que le pertoca
+    console.log(name);
+    console.log(value);
+  }
   return (
     <div className="register__participant">
-      <h3 className="register__title">
+      <h3 className="participant__title register__title">
         {text}
         {' '}
         {participantNumber}
@@ -13,7 +22,25 @@ function Participant({ text, participantNumber, isCaptain }) {
         {' '}
         {isCaptain ? 'Capitán' : ''}
       </h3>
+      <div className="participant__inputs">
+        <div className="inputs__entries">
+          <div className="entries__name">
+            <Input type="text" name="participantName" placeholder="Nombre del participante*" value="" onChange={handleTextInputChange} />
+          </div>
+          <div className="entries__surname">
+            <Input type="text" name="participantSurname" placeholder="Apellido del participante*" value="" onChange={handleTextInputChange} />
+          </div>
+        </div>
 
+        <div className="inputs__entries">
+          <div className="entries__mail">
+            <Input type="email" name="participantMail" placeholder="e-mail*" value="" onChange={handleTextInputChange} />
+          </div>
+          <div className="entries__number">
+            <Input type="number" name="participantNumber" placeholder="Teléfono*" value="" onChange={handleTextInputChange} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- front-developers-games/src/views/Register/Register.jsx
+++ front-developers-games/src/views/Register/Register.jsx
@@ -24,10 +24,10 @@ const participantInitialState = {
 };
 
 function Register() {
-  const [teamRegister, setTeamRegister] = useState(teamInitialState);
-  const [participantRegister, setParticipantRegister] = useState(participantInitialState);
-  const [participantsCounter, setParticipantsCounter] = useState(0);
-  // TODO: crear warningMessage
+  // const [teamRegister, setTeamRegister] = useState(teamInitialState);
+  // const [participantRegister, setParticipantRegister] = useState(participantInitialState);
+  const [participantsCounter, setParticipantsCounter] = useState(1);
+  const [warningMessage, setWarningMessage] = useState('');
   function handleTextInputChange({ target: { name, value } }) {
     //   TODO: guardar el valor en el state que le pertoca
     console.log(name);
@@ -45,10 +45,10 @@ function Register() {
           <div className="form__input">
             <Input type="text" name="name" placeholder="Nombre del equipo*" value={teamInitialState.name} onChange={handleTextInputChange} />
           </div>
-          <div className="form__entry-photo">
+          {/* <div className="form__entry-photo">
             <img className="entry-photo__image" src={CamaraIcon} alt="Insert photo" />
             <p className="entry-photo__text">Sube la foto de tu equipo</p>
-          </div>
+          </div> */}
           <Participant text="Participante" participantNumber={participantsCounter} isCaptain />
         </form>
       </section>
