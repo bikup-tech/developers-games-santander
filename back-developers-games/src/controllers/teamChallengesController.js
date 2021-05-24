148657841e2bcf4939cf6ad078814dc607685e01
gerardramonp <gerardramonp@gmail.com>
2021-05-24 12:06:51 +0200

    Remove console logs

--- back-developers-games/src/controllers/teamChallengesController.js
+++ back-developers-games/src/controllers/teamChallengesController.js
@@ -14,9 +14,7 @@ function teamChallengesController() {
       if (!teamId || teamId === 'undefined') {
         throw new CustomError(BAD_REQUEST, MISSING_QUERY_PROPERTIES('teamId'));
       }
-      console.log(teamId);
       const foundTeamChallenges = await teamChallengeService.findTeamChallenges(teamId);
-      console.log(foundTeamChallenges);
       return handleResponseSuccess(res, foundTeamChallenges);
     } catch (getTeamChallengesError) {
       return handleResponseError(res, getTeamChallengesError);
