61377741031ef96212d8299ba0211a2b65696754
gerardramonp <gerardramonp@gmail.com>
2021-05-28 12:18:09 +0200

    Create terms view structure & route

---
+++ front-developers-games/src/views/TermsAndConditions/TermsAndConditions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import AppWrapper from '../../components/AppWrapper/AppWrapper';
+
+function TermsAndConditions() {
+  return (
+    <AppWrapper title="Terms & Conditions">
+      asd
+    </AppWrapper>
+  );
+}
+
+export default TermsAndConditions;
