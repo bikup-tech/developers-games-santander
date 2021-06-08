6a8746a9ff80421e680c923fbab9e8175af8d58b
gerardramonp <gerardramonp@gmail.com>
2021-06-08 15:39:13 +0200

    Clear initialState

--- front-developers-games/src/redux/store/initialState.js
+++ front-developers-games/src/redux/store/initialState.js
@@ -1,9 +1,9 @@
 export default {
   mainReducer: {
     tournamentId: '60be036d5695a3805e903f91',
-    participants: ['gerard'],
+    participants: [],
     team: {
-      name: 'bikUP',
+      name: '',
     },
     alert: { type: '', message: '' },
     toLoadChallengeDetail: localStorage.getItem('toLoadChallengeDetail') || '',
@@ -37,7 +37,7 @@ export default {
     user: {
       isLogged: false,
       userLogged: {
-        name: 'gerard', _id: '60b4f66b4562c2e7a047f928', isCaptain: true, isAdmin: false,
+        name: '', _id: '', isCaptain: false, isAdmin: false,
       },
     },
   },
