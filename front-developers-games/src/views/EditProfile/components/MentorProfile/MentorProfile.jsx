c917d1e5eb3cb1517c25469fee11c1d263451d05
gerardramonp <gerardramonp@gmail.com>
2021-06-16 13:07:32 +0200

    remove eslint rules

--- front-developers-games/src/views/EditProfile/components/MentorProfile/MentorProfile.jsx
+++ front-developers-games/src/views/EditProfile/components/MentorProfile/MentorProfile.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
