28523142e15e56bca227ff8752f16dbae3458b45
mourad5000 <mouradmounim7@gmail.com>
2021-06-17 16:25:53 +0200

    Add buttons to modal

--- front-developers-games/src/components/CreateParticipantModal/CreateParticipantModal.jsx
+++ front-developers-games/src/components/CreateParticipantModal/CreateParticipantModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 // TODO: crear el modal con el FORMIK
@@ -11,7 +12,7 @@ function CreateParticipantModal({ userRol, userNumber, isFormVisible }) {
   return (
     // <section className={`${isFormVisible > 0 && 'modal__add-participant'}`}>
     <section className="modal__add-participant">
-      <div className="add-participant__opacity" />
+      <div className="add-participant__opacity" onClick={() => { console.log('hola'); }} />
       <form className="add-participant__form">
         <h3 className="participant__title app__title">
           {userRol}
@@ -46,12 +47,19 @@ function CreateParticipantModal({ userRol, userNumber, isFormVisible }) {
             placeholder={`${userRol} phone*`}
           />
         </div>
-        <div className=" form__input--modal profile-button-container">
-          <MainButton>
-            Save
-            {' '}
-            {userRol}
-          </MainButton>
+        <div className="form__buttons">
+          <div className="profile-button-container mb-12--mobile">
+            <MainButton>
+              Save
+              {' '}
+              {userRol}
+            </MainButton>
+          </div>
+          <div className="profile-button-container">
+            <MainButton isSecondary>
+              Cancel
+            </MainButton>
+          </div>
         </div>
       </form>
     </section>
