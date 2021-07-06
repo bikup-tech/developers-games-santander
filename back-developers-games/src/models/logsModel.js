807256b374b675520a33b439da0cd9f4031fcdde
gerardramonp <gerardramonp@gmail.com>
2021-07-06 16:00:31 +0200

    Add date to logModel

--- back-developers-games/src/models/logsModel.js
+++ back-developers-games/src/models/logsModel.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const logsModel = new Schema({
   data: { type: Object },
+  date: { type: String },
 });
 
 module.exports = model('Log', logsModel);
