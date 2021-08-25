9bb6538f84740dfee605ac7f44e3953cd85438e5
mourad5000 <mouradmounim7@gmail.com>
2021-08-25 18:08:10 +0200

    Add links to mainbutton in cared

--- front-developers-games/src/views/HandsOnWorkshops/HandsOnWorkshops.jsx
+++ front-developers-games/src/views/HandsOnWorkshops/HandsOnWorkshops.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
 
 import './HandsOnWorkshops.scss';
 
 // constants
 import workshops from '../../constants/workshops';
 
+// Images
+import teamIcon from '../../assets/images/team-icon.svg';
+import kitIcon from '../../assets/images/kit-icon.svg';
+import guidesIcon from '../../assets/images/guides-icon.svg';
+
 // components
 import AppWrapper from '../../components/AppWrapper/AppWrapper';
 import WorkShopCard from './components/WorkShopCard';
+import MainButton from '../../components/MainButton/MainButton';
 
 function HandsOnWorkshops() {
+  const { user } = useSelector(({ authReducer }) => authReducer);
   return (
     <AppWrapper title="Hands-on Workshops">
       <div className="workshop-container">
@@ -22,6 +31,36 @@ function HandsOnWorkshops() {
               />
             ))
         }
+        <div className="challenges__actions">
+          {(user.userLogged.role <= 1 && user.isLogged) && (
+          <>
+            <div className="actions__welcome-kit">
+              <MainButton isSecondary>
+                <a href="https://events.redhat.com/profile/395144" target="_blank" className="welcome-kit__link" rel="noreferrer">
+                  <img src={kitIcon} alt="welcome kit" className="link__image" />
+                  <span className="link__text">Request your welcome kit</span>
+                </a>
+              </MainButton>
+            </div>
+            <div className="actions__welcome-kit">
+              <MainButton isSecondary>
+                <Link to="/profile" className="edit-team__link">
+                  <img src={teamIcon} alt="edit team" className="link__image" />
+                  <span className="link__text">Edit your profile</span>
+                </Link>
+              </MainButton>
+            </div>
+          </>
+          )}
+          <div className="actions__edit-team">
+            <MainButton isSecondary>
+              <NavLink to="/participantsGuide">
+                <img src={guidesIcon} alt="edit team" className="link__image" />
+                <span className="link__text">Participant guide</span>
+              </NavLink>
+            </MainButton>
+          </div>
+        </div>
       </div>
     </AppWrapper>
   );
--- front-developers-games/src/views/HandsOnWorkshops/components/WorkShopCard.jsx
+++ front-developers-games/src/views/HandsOnWorkshops/components/WorkShopCard.jsx
@@ -9,13 +9,13 @@ import MainButton from '../../../components/MainButton/MainButton';
 function WorkShopCard({ workShop }) {
   return (
     <div className="workShop-card">
-      <h3 className="challenge__subtitle">{workShop.name}</h3>
+      <h3 className="challenge__subtitle--workshop">{workShop.name}</h3>
       <p className="challenge__author">{workShop.authors}</p>
       <p className="challenge__date">{workShop.date}</p>
       <p className="challenge__author">{workShop.seats}</p>
       <div className="challenge__regiter-button">
         <MainButton>
-          Register
+          <a href={workShop.link} target="_blank" rel="noreferrer">Register</a>
         </MainButton>
       </div>
       <div className="challenge__separator" />
