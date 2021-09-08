d7e8903c86bfc1c721074dbe30d2e155fed9eaeb
mourad5000 <mouradmounim7@gmail.com>
2021-09-08 19:45:53 +0200

    Create FinishedRegistration compo

---
+++ front-developers-games/src/views/FinishedRegistration/FinishedRegistration.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// components
+import AppWrapper from '../../components/AppWrapper/AppWrapper';
+
+function FinishedRegistration() {
+  return (
+    <AppWrapper title="Registration time ended">
+      <section className="register-container">
+        <h3 className="app__title">Santander & Red Hat Developer Games 2021 Registration ended</h3>
+      </section>
+    </AppWrapper>
+  );
+}
+
+export default FinishedRegistration;
