08285bf67d511d1bd4cc17308dfd6b5ab8322e04
mourad5000 <mouradmounim7@gmail.com>
2021-09-09 12:05:53 +0200

    change finishedRegistration html

--- front-developers-games/src/views/FinishedRegistration/FinishedRegistration.jsx
+++ front-developers-games/src/views/FinishedRegistration/FinishedRegistration.jsx
@@ -7,7 +7,18 @@ function FinishedRegistration() {
   return (
     <AppWrapper title="Registration time ended">
       <section className="register-container">
-        <h3 className="app__title">Santander & Red Hat Developer Games 2021 Registration ended</h3>
+        <h3 className="app__title normal__font-size">
+          The registration  is closed! Stay tunned for upcoming events with Red Hat!
+          Join us in our Red Hat developer site to get the latest news.
+          <a
+            href="https://developers.redhat.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="app__title normal__font-size"
+          >
+            (https://developers.redhat.com/)
+          </a>
+        </h3>
       </section>
     </AppWrapper>
   );
